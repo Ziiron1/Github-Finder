9a6c3f5bd29bf19e4c63ab5911ba1583acd5f784
Ziiron1 <guilherme_profissional@yahoo.com.br>
2022-12-25 01:41:23 -0300

    Update v1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
     <div>
-
       <h1>GitHub Finder</h1>
-
+      <Outlet />
     </div>
-  )
+  );
 }
 
 export default App
